a0222a9bc72aca8177a9c3968498ceeb79a1c270
Leonardo Rocco <l.rocco@ifsp.edu.br>
2025-05-30 19:57:40 -0300

    Add nodemon e scripts

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.write('Hello, World! Pela Web!');
+app.get('/', (req, res) => {;
+  res.write('Hello, World! Pela Web! :)');
   res.end();
 }
 );
