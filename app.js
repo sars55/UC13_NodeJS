b1dca355b393be3d588a30f6d11cb53e61615cba
Leonardo Rocco <l.rocco@ifsp.edu.br>
2025-06-03 20:16:10 -0300

    Extensão MySQL2 e Conexão com o Banco

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
+const conexao = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'senac',
+  port: 3306,
+  database: 'ecommerce_pc'
+});
+
+conexao.connect((erro) => {
+  if (erro) {
+    console.error('😫 Erro ao conectar ao banco de dados:', erro);
+    return;
+  }
+  console.log('😁 Conexão com o banco de dados estabelecida com sucesso!');
+});
 
 app.get('/', (req, res) => {;
   res.write('Hello, World! Pela Web! :)');
