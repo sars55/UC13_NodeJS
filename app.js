ac041793591a5468198a25cf54d67a51b9bb085d
Leonardo Rocco <l.rocco@ifsp.edu.br>
2025-06-04 18:13:41 -0300

    Instalação e configuração do Bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { engine } = require('express-handlebars');
 
 const mysql = require('mysql2');
 
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
@@ -25,8 +27,29 @@ conexao.connect((erro) => {
 });
 
 app.get('/', (req, res) => {;
-  res.render('teste');
+  let sql = 'SELECT * FROM produtos';
+  conexao.query(sql, function (erro, produtos_qs) {
+    if (erro) {
+      console.error('😫 Erro ao consultar produtos:', erro);
+      res.status(500).send('Erro ao consultar produtos');
+      return;
+    }
+    res.render('index', { produtos: produtos_qs });
+  });
 }
 );
+
+
+app.get('/clientes', (req, res) => {;
+  let sql = 'SELECT * FROM clientes';
+  conexao.query(sql, function (erro, clientes_qs) {
+    if (erro) {
+      console.error('😫 Erro ao consultar clientes:', erro);
+      res.status(500).send('Erro ao consultar clientes');
+      return;
+    }
+    res.render('clientes', { clientes: clientes_qs });
+  });
+});
 
 app.listen(8080);
